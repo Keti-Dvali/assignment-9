df4865f063dc0ec97620e50d9db0eaf5cba7d581
Keti-Dvali <104909714+Keti-Dvali@users.noreply.github.com>
2022-08-09 11:18:58 -0700

    finished

---
+++ static/javascript/main.js
@@ -0,0 +1,9 @@
+let User = {
+    name: 'Keti',
+    surName:'Dvali', 
+    age: 22,
+    isMarried: false,
+    programmingLanguage: 'javascript',
+    isWillingToLearnFrontEnd: true, //:)) 
+}
+console.log(User.name, User.surName, User.age, User.isMarried, User.programmingLanguage, User.isWillingToLearnFrontEnd);
